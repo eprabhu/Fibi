e2ac36cc59705bcd5d33576f77d149c2dfb75101
LijinWilsonPolus <128453016+LijinWilsonPolus@users.noreply.github.com>
2023-03-28 12:42:11 +0530

    FC-18, FC-20
    FC-18: Add relationship modal creation;
    FC-20: Entity list created;

---
+++ Client/projects/coi/src/app/disclosure/sfi/Add-relationship-modal/Add-relationship-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Add-relationship-modal',
+  templateUrl: './Add-relationship-modal.component.html',
+  styleUrls: ['./Add-relationship-modal.component.scss']
+})
+export class AddRelationshipModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
