91e5a8f803ff090e7fc1c5af2b284025f538d1a3
SarumathiPolus <sarumathi.n@polussoftware.com>
2023-03-31 13:21:09 +0530

    FC-31 and FC-32
    Completed changes in relationship screen
    Added bootstrap and types & bootstrap

--- Client/projects/coi/src/app/disclosure/relationship/relationship.component.ts
+++ Client/projects/coi/src/app/disclosure/relationship/relationship.component.ts
@@ -1,31 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { slideInOut, fadeDown, slideHorizontalOverlay, slideHorizontal } from '../../../../../fibi/src/app/common/utilities/animations';
+import { DataStoreService } from '../services/data-store.service';
+import { RelationshipService } from './relationship.service';
 
 @Component({
   selector: 'app-relationship',
   templateUrl: './relationship.component.html',
-  styleUrls: ['./relationship.component.css']
+  styleUrls: ['./relationship.component.scss'],
+  animations: [slideHorizontal],
 })
-
 export class RelationshipComponent {
   isShowRelation = false;
   proposalArray = [];
+  coiStatusList = [];
+  searchText: any;
+  searchResult = [];
+  moduleId: any;
+  moduleCode: any;
+  selectedProject: any;
+  coiData: any;
+  selectedProjectForView: any;
+
+  constructor(private _relationShipService: RelationshipService,private _route: ActivatedRoute,
+    private _dataStore: DataStoreService,public _router: Router) { }
+
   closePage() {
     this.isShowRelation = false;
+    this.moduleCode = null;
+    this.moduleId = null;
+    this.loadProjectRelations();
   }
 
   ngOnInit() {
-    let selectedProject = {
-      title: '#11111-Enginnering of sustainable and adaptable 3D-printed material',
-      principalInvestigator: 'Smith, Will',
-      sponsor: 'Air Force',
-      startDate: '27/02/2012',
-      endDate: '27/02/2022',
-      primeSponsor: 'Aerospace Nation',
-      moduleStatus: 'In Progress',
-      leadUnit: '100000 - RMIT',
-    }
-    this.proposalArray.push(selectedProject);
-    this.proposalArray.push(selectedProject);
-    this.proposalArray.push(selectedProject);
+    this.getDataFromStore();
+    this.loadProjectRelations();
   }
+
+  getDisclosureCount(typeCode, disclosureStatus) {
+    if(disclosureStatus) {
+      let value = disclosureStatus.find(ele => Object.keys(ele) == typeCode);
+      return value ? value[typeCode] : 0;
+    }
+  }
+
+  private getDataFromStore() {
+    this.coiData = this._dataStore.getData();
+    console.log(this.coiData)
+}
+  loadProjectRelations() {
+    this._relationShipService.getProjectRelations(this.coiData.coiDisclosure.disclosureId, this.coiData.coiDisclosure.disclosureStatusCode).subscribe((data: any) => {
+      this.proposalArray = data.awards;
+      data.proposals.every(ele => this.proposalArray.push(ele));
+      this.coiStatusList = data.coiDisclosureDetailStatuses;
+    });
+  }
+
+  openDefineRelationship(test) {
+    this.moduleCode = test.moduleCode;
+    this.moduleId = test.moduleItemId ;
+    this.selectedProject = test;
+    this.isShowRelation = true;
+  }
+
 }
---
+++ Client/projects/coi/src/app/shared-components/no-information/no-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-information',
+  templateUrl: './no-information.component.html',
+  styleUrls: ['./no-information.component.scss']
+})
+export class NoInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
