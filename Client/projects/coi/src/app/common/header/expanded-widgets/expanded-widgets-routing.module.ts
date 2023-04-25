2e56beaf95c0b8f7f6895028bc275f9e719a9435
aby-ponnachan <aby.p@polussoftware.com>
2023-04-25 17:53:51 +0530

    design changes in action list,action list search modale,add relations,back-to -award list button routing
    design changes in action list,action list search modale,add relationship modale,back-to -award list button routing

--- Client/projects/coi/src/app/common/header/expanded-widgets/expanded-widgets-routing.module.ts
+++ Client/projects/coi/src/app/common/header/expanded-widgets/expanded-widgets-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExpandedActionListComponent } from 'projects/fibi/src/app/expanded-widgets/expanded-action-list/expanded-action-list.component';
+import { UserDashboardComponent } from '../../../user-dashboard/user-dashboard.component';
 
 
 const routes: Routes = [
   {
     path: 'expanded-widgets', component: ExpandedActionListComponent,
-  }];
+  },
+ {
+  path: 'user-dashboard', component: UserDashboardComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
