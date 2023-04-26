7ebb1b34aebc5d90df7f95495be1fa9ca1f814f8
aby-ponnachan <aby.p@polussoftware.com>
2023-04-26 18:17:27 +0530

    admin dashboard advanced search design changes ,date format change in add SFI & admin dashboard advanced search
    admin dashboard advanced search design changes ,date format change in add SFI & admin dashboard advanced search

--- Client/projects/coi/src/app/shared/add-sfi/add-sfi.component.ts
+++ Client/projects/coi/src/app/shared/add-sfi/add-sfi.component.ts
@@ -9,6 +9,7 @@ import { hideModal } from '../../../../../fibi/src/app/common/utilities/custom-u
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { getDateObjectFromTimeStamp, parseDateWithoutTimestamp } from '../../../../../fibi/src/app/common/utilities/date-utilities';
+import { DEFAULT_DATE_FORMAT } from 'projects/fibi/src/app/app-constants';
 
 export interface EndpointOptions {
   contextField: string;
@@ -39,6 +40,7 @@ export class AddSfiComponent implements OnInit {
   isAddAttachment = false;
   isAddAssignee = false;
   dateTime: string;
+  datePlaceHolder = DEFAULT_DATE_FORMAT;
   isReadMore: false;
   showRelationshipModal = false;
   clearField: any = false;
