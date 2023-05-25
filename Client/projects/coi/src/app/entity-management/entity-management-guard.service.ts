df33e177524209f9fd1e92ca3bee3f4056b420df
aby-ponnachan <aby.p@polussoftware.com>
2023-05-25 10:30:01 +0530

    rights condition errors fixed( entity management)
    rights condition errors fixed( entity management)

--- Client/projects/coi/src/app/entity-management/entity-management-guard.service.ts
+++ Client/projects/coi/src/app/entity-management/entity-management-guard.service.ts
@@ -11,26 +11,19 @@ export class EntityManagementGuardService implements CanActivate{
 rightList:string[]=[]
 
 
-  rights() {
-    this._commonService.fetchPermissions().then((right:any)=>{
-       this.rightList=right;
-      }).catch((error) => {
-        console.log('Error fetching permissions:', error);
-   })
-  }
-
 constructor(private router: Router,
   private _commonService: CommonService) { }
   
 canActivate(
-): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+): boolean{
 
-  if (this._commonService.rightList.includes('MANAGEENTITY') || this._commonService.rightList.includes('VIEWENTITY')) {
+  if (this._commonService.rightsArray.includes('MANAGE_ENTITY') || this._commonService.rightsArray.includes('VIEW_ENTITY')) {
     return true;
   } else {
        this.router.navigate(['/coi/error-handler/401']);
        return false;
   }
 }
+
 }
 
