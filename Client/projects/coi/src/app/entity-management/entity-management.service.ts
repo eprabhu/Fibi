d21615827743d96dd35e08ed0ae6df9a9fc725ee
aby-ponnachan <aby.p@polussoftware.com>
2023-07-04 10:15:40 +0530

    confirmation modal design changes, manage entity
    FC-260 Entity management sort
    FC-264 confirmation modal design changes

--- Client/projects/coi/src/app/entity-management/entity-management.service.ts
+++ Client/projects/coi/src/app/entity-management/entity-management.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CommonService } from '../common/services/common.service';
+import { NameObject } from '../admin-dashboard/admin-dashboard.service';
+
 
 @Injectable(
   {providedIn: 'root'}
@@ -10,9 +12,12 @@ export class EntityManagementService {
 
   constructor(private _commonService: CommonService, private _http: HttpClient) { }
 
+  sort: any;
   coiRequestObject = new EntityDashboardRequest();
   entityDashDefaultValues = new EntityDashDefaultValues();
   isShowEntityNavBar = false;
+  searchDefaultValues: NameObject = new NameObject();
+  sortCountObject: SortCountObj = new SortCountObj();
   relationshipDashboardRequest = new RelationshipDashboardRequest();
   statusCodes: any = [
     {code: 'Y', description: 'Active'},
@@ -57,9 +62,7 @@ export class EntityDashboardRequest {
   property22 = [];
   property18 = false;
   property19 = false;
-  sort: any = {
-    sortBy : 'updateTimeStamp'
-  };
+  sort: any = {'updateTimeStamp': 'desc'};
   tabName = '';
   advancedSearch = 'L';
   currentPage = 1;
@@ -71,8 +74,7 @@ export class RelationshipDashboardRequest {
   property3 = null;
   property4 = [];
   property5 = null;
-  sort: any = {
-  };
+  sort: any = {'updateTimeStamp': 'desc'};
   filterType = null;
   currentPage = 1;
   pageNumber = 20;
@@ -82,4 +84,12 @@ export class RelationshipDashboardRequest {
 export class EntityDashDefaultValues {
   entitySearch = '';
   countrySearch = '';
+}
+
+export class SortCountObj {
+  name = 0;
+  entityType = 0;
+  riskLevel = 0;
+  country = 0;
+  updateTimeStamp = 2;
 }
