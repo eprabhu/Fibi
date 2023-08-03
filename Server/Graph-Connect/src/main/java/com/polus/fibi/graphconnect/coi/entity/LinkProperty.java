4c7a39244bebb04f33aac6d5bc6542b9ee710faa
anishantonyk <itzanishkantony@gmail.com>
2023-08-03 09:56:20 +0100

    Configuring Graph Connect application to COI Mircoservices

---
+++ Server/Graph-Connect/src/main/java/com/polus/fibi/graphconnect/coi/entity/LinkProperty.java
@@ -0,0 +1,28 @@
+package com.polus.fibi.graphconnect.coi.entity;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.RelationshipProperties;
+import org.springframework.data.neo4j.core.schema.TargetNode;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RelationshipProperties
+public class LinkProperty {
+    
+	@Id
+    @GeneratedValue
+    private Long id;
+    private Long elementId;    
+    private Long startNodeElementId;    
+    private Long endNodeElementId;
+    
+
+    @TargetNode
+    private Country targetNode;
+}
