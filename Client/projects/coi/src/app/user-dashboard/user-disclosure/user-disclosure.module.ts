6d8bd4f7a338186f792d408aa88906597b18aeec
Ayush Mahadev <ayush.mahadev@polussolutions.com>
2023-09-28 20:39:42 +0530

    SFI View slider issue fix
    • corrected inefficient usage of variables in common slider.

--- Client/projects/coi/src/app/user-dashboard/user-disclosure/user-disclosure.module.ts
+++ Client/projects/coi/src/app/user-dashboard/user-disclosure/user-disclosure.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterModule, Routes} from "@angular/router";
-import {MatIconModule} from "@angular/material/icon";
-import {UserDisclosureService} from "./user-disclosure.service";
-import {SharedComponentModule} from "../../shared-components/shared-component.module";
-import {FormsModule} from "@angular/forms";
-import { SharedModule } from '../../shared/shared.module';
-import { UserDisclosureComponent } from './user-disclosure.component';
-import { DisclosureHistoryCardComponent } from './disclosure-history-card/disclosure-history-card.component';
+import {RouterModule, Routes} from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {UserDisclosureService} from './user-disclosure.service';
+import {SharedComponentModule} from '../../shared-components/shared-component.module';
+import {FormsModule} from '@angular/forms';
+import {SharedModule} from '../../shared/shared.module';
+import {UserDisclosureComponent} from './user-disclosure.component';
+import {DisclosureHistoryCardComponent} from './disclosure-history-card/disclosure-history-card.component';
+import {EntityDetailsModule} from '../../disclosure/entity-details/entity-details.module';
 
 const routes: Routes = [{path: '', component: UserDisclosureComponent}];
 
@@ -23,7 +24,8 @@ const routes: Routes = [{path: '', component: UserDisclosureComponent}];
         MatIconModule,
         SharedModule,
         SharedComponentModule,
-        FormsModule
+        FormsModule,
+        EntityDetailsModule
     ]
 })
 export class UserDisclosureModule {
