6167622893b26aff5820cb66da7e4b52d0f19e9b
Ayush Mahadev <ayush.mahadev@polussolutions.com>
2023-09-28 13:39:13 +0530

    COI Header updated v2
    • added person search option.

--- Client/projects/coi/src/app/common/left-nav-bar/left-nav-bar.component.ts
+++ Client/projects/coi/src/app/common/left-nav-bar/left-nav-bar.component.ts
@@ -1,18 +1,29 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {environment} from '../../../environments/environment';
+import {CommonService} from "../services/common.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-left-nav-bar',
     templateUrl: './left-nav-bar.component.html',
     styleUrls: ['./left-nav-bar.component.scss']
 })
-export class LeftNavBarComponent {
+export class LeftNavBarComponent implements OnInit {
     deployMap = environment.deployUrl;
     isNavExpanded = false;
+    isAdministrator = false;
     @ViewChild('sideBarMenu', {static: true}) sideBarMenu: ElementRef;
 
-    constructor() {
+    constructor(private _commonService: CommonService, private _router: Router) {
         document.addEventListener('mouseup', this.offClickSideBarHandler.bind(this));
+    }
+
+    ngOnInit() {
+        this.isAdministrator = this._commonService.getAvailableRight(['COI_ADMINISTRATOR', 'VIEW_ADMIN_GROUP_COI'])
+            || this._commonService.isCoiReviewer;
+        if (this.isAdministrator) {
+            this._router.navigate(['/coi/admin-dashboard'], { queryParamsHandling: 'preserve' });
+        }
     }
 
     offClickSideBarHandler(event) {
