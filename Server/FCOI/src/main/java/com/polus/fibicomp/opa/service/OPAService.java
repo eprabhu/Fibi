cd20c62cb2df026a3063f8036506b0fd3a61f41e
Kiran Kumar S <kiran.ks@polussoftware.com>
2023-10-05 15:18:49 +0530

    OPA Code base initial commit

---
+++ Server/FCOI/src/main/java/com/polus/fibicomp/opa/service/OPAService.java
@@ -0,0 +1,18 @@
+package com.polus.fibicomp.opa.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OPAService {
+
+	/**
+	 * This service is used to check if OPA disclosure has to be submitted for the logged in user
+	 * @param personId 
+	 * @param CreateOpaDto
+	 */
+	Boolean isOpaDisclosureRequired(String personId);
+
+	ResponseEntity<Object> createOpaDisclosure(String personId, String homeUnit);
+
+}
