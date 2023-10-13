da070d25e0d0c753ce29aea6ca3b502010d94c40
arunrajpolus <52557341+arunrajpolus@users.noreply.github.com>
2023-10-13 12:04:34 +0530

    Notes & Attachments - Attachments - Initial Commit
    Jira Link :- https://polussolutions.atlassian.net/browse/FC-482
    
    * Added api s for saving and fetching attachments
    * Introduced a new table attachments for saving all the entries
    
    Changes not part of this ticket :-
    
    * Added an api for deleting a note
    * Removed Alert type and expiration date from 'ADD_TO_INBOX' procedure
    
    Reviewer:- Arjun K R

---
+++ Server/FCOI/src/main/java/com/polus/fibicomp/coi/pojo/Attachments.java
@@ -0,0 +1,80 @@
+package com.polus.fibicomp.coi.pojo;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "ATTACHMENTS")
+public class Attachments implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "ATTACHMENT_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer attachmentId;
+	
+	@ManyToOne(optional = true)
+	@JoinColumn(foreignKey = @ForeignKey(name = "ATTACHMENTS1_FK1"), name = "ATTA_TYPE_CODE", referencedColumnName = "ATTA_TYPE_CODE", insertable = false, updatable = false)
+	private DisclAttaType disclAttaTypeDetails;
+	
+	@Column(name = "PERSON_ID")
+	private String personId;
+	
+	@Column(name = "ATTA_TYPE_CODE")
+	private String attaTypeCode;
+
+	@Column(name = "FILE_NAME")
+	private String fileName;
+
+	@Column(name = "MIME_TYPE")
+	private String mimeType;
+
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	@Column(name = "FILE_DATA_ID")
+	private String fileDataId;
+
+    @CreatedBy
+    @Column(name = "CREATE_USER")
+    private String createUser;
+
+    @CreatedDate
+    @Column(name = "CREATE_TIMESTAMP")
+    private Timestamp createTimestamp;
+
+	@LastModifiedBy
+	@Column(name = "UPDATE_USER")
+	private String updateUser;
+
+	@LastModifiedDate
+	@Column(name = "UPDATE_TIMESTAMP")
+	private Timestamp updateTimestamp;
+
+}
