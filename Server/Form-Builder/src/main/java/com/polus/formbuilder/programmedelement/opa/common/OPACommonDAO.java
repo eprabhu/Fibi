0202c61663da2eebd52bc7463666d6938b6cfb01
Kiran Kumar S <kiran.ks@polussoftware.com>
2024-02-12 09:35:15 +0530

    Commit for PE data sync issue

--- Server/Form-Builder/src/main/java/com/polus/formbuilder/programmedelement/opa/common/OPACommonDAO.java
+++ Server/Form-Builder/src/main/java/com/polus/formbuilder/programmedelement/opa/common/OPACommonDAO.java
@@ -33,7 +33,7 @@ public class OPACommonDAO {
 
 	
 	@Transactional
-	public void personSyncOPAPersonEntityAction(Integer opaDisclosureId, String updateUser) throws InterruptedException {
+	public void personSyncOPAPersonEntityAction(Integer opaDisclosureId, String updateUser) {
 		StoredProcedureQuery query = entityManager.createStoredProcedureQuery("PROC_SYNC_OPA_PER_ENTITY")
 				.registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN)
 				.registerStoredProcedureParameter(2, String.class, ParameterMode.IN);
@@ -42,7 +42,7 @@ public class OPACommonDAO {
 		query.setParameter(2, updateUser);
 		query.execute();
 		Long result = (Long) query.getSingleResult();
-		Thread.sleep(2000);
+		entityManager.flush();
 	}
 
 	@SuppressWarnings("unchecked")
