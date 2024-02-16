5ae015e33e0fce437150a5289d33a6ea498cdcc4
SarumathiPolus <72390623+SarumathiPolus@users.noreply.github.com>
2024-02-16 18:24:48 +0530

    Null check
    Issue fix - null check and no data indication -- added.

--- Client/projects/coi/src/app/shared/form-builder-view/PE-components/OPA-institute-resources/OPA-institute-resources.component.ts
+++ Client/projects/coi/src/app/shared/form-builder-view/PE-components/OPA-institute-resources/OPA-institute-resources.component.ts
@@ -93,15 +93,17 @@ export class OPAInstituteResourceUseComponent implements OnInit {
     }
 
     getEntityRelationTypePills(validPersonEntityRelType: string) {
-        if (this.relationshipTypeCache[validPersonEntityRelType]) {
+        if (validPersonEntityRelType) {
+            if (this.relationshipTypeCache[validPersonEntityRelType]) {
+                return this.relationshipTypeCache[validPersonEntityRelType];
+            }
+            const entityRelTypes = validPersonEntityRelType.split(':;:');
+            this.relationshipTypeCache[validPersonEntityRelType] = entityRelTypes.map(entity => {
+                const relationshipType = entity.split(':');
+                return { relationshipType: relationshipType[0] || '', description: relationshipType[1] || '' };
+            });
             return this.relationshipTypeCache[validPersonEntityRelType];
         }
-        const entityRelTypes = validPersonEntityRelType.split(':;:');
-        this.relationshipTypeCache[validPersonEntityRelType] = entityRelTypes.map(entity => {
-            const relationshipType = entity.split(':');
-            return { relationshipType: relationshipType[0] || '', description: relationshipType[1] || '' };
-        });
-        return this.relationshipTypeCache[validPersonEntityRelType];
     }
 
     editEntityItem(outsideFinRelationData: any , index): void {
