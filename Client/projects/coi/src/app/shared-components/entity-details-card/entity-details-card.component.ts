d202ae7a78ba745afd42154adacbfb533cbcb7f9
SarumathiPolus <sarumathi.n@polussoftware.com>
2024-06-04 15:28:22 +0530

    Missed Changes in pop-up redesign
    1. Arrow missing in Attachment dropdown.
    2. Tooltip changes in leave page modals of SFI.
    3. Added fullstop and one more help text in create disclosure popup.
    4. Added ... ellipse for large entity name and country in entity card.
    5. added class for the same.
    6. Increase font weight of ...
    7. removed word break in text-slice.
    REviewed by @AbdulAshfaque
    Build Success.

--- Client/projects/coi/src/app/shared-components/entity-details-card/entity-details-card.component.ts
+++ Client/projects/coi/src/app/shared-components/entity-details-card/entity-details-card.component.ts
@@ -1,51 +1,57 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { isEmptyObject } from 'projects/fibi/src/app/common/utilities/custom-utilities';
 
+class CardDetails {
+    entityName: string = '';
+    entityType: string = '';
+    countryName: string = '';
+    entityStatus: string = '';
+    relationship: any = {};
+}
 @Component({
-  selector: 'app-entity-details-card',
-  templateUrl: './entity-details-card.component.html',
-  styleUrls: ['./entity-details-card.component.scss']
+    selector: 'app-entity-details-card',
+    templateUrl: './entity-details-card.component.html',
+    styleUrls: ['./entity-details-card.component.scss']
 })
 export class EntityDetailsCardComponent implements OnInit {
 
-  @Input() entityDetails: any;
-  @Input() relationshipDetails: any = {};
+    @Input() entityDetails: any;
+    @Input() relationshipDetails: any = {};
 
-  cardDetails: any = {};
-  constructor() { }
+    cardDetails: CardDetails = new CardDetails();
 
-  ngOnInit() {
-    this.setEntityCardDetails();
-  }
-
-  setEntityCardDetails() {
-    this.cardDetails.entityName = this.entityDetails.entityName;
-    this.cardDetails.entityType = (this.entityDetails && this.entityDetails.entityType) ? this.entityDetails.entityType.description : '';
-    this.cardDetails.countryName = (this.entityDetails && this.entityDetails.country) ? this.entityDetails.country.countryName : '';
-    this.cardDetails.entityStatus = this.entityDetails ? this.entityDetails.isActive ? 'Active' : 'Inactive' : '';
-    if(this.checkForRelationShipDetails()) {
-        this.cardDetails.relationship =(this.groupBy( this.relationshipDetails, "coiDisclosureType", "description"));
+    ngOnInit() {
+        this.setEntityCardDetails();
     }
-  }
 
-  groupBy(jsonData, key, innerKey) {
-    return jsonData.reduce((relationsTypeGroup, item) => {
-        (relationsTypeGroup[item[key][innerKey]] = relationsTypeGroup[item[key][innerKey]] || []).push(item);
-        return relationsTypeGroup;
-    }, {});
-  }
-
-  checkForRelationShipDetails() {
-    return !isEmptyObject(this.relationshipDetails);
-  }
-
-  getIcon(key) {
-    switch(key) {
-        case 'Commitment': return 'handshake';
-        case 'Travel': return 'flight';
-        case 'Financial': return 'paid';
-        default: return;
+    setEntityCardDetails() {
+        this.cardDetails.entityName = this.entityDetails.entityName;
+        this.cardDetails.entityType = (this.entityDetails && this.entityDetails.entityType) ? this.entityDetails.entityType.description : '';
+        this.cardDetails.countryName = (this.entityDetails && this.entityDetails.country) ? this.entityDetails.country.countryName : '';
+        this.cardDetails.entityStatus = this.entityDetails ? this.entityDetails.isActive ? 'Active' : 'Inactive' : '';
+        if (this.checkForRelationShipDetails()) {
+            this.cardDetails.relationship = (this.groupBy(this.relationshipDetails, "coiDisclosureType", "description"));
+        }
     }
-  }
+
+    groupBy(jsonData: any, key: string, innerKey: string): any {
+        return jsonData.reduce((relationsTypeGroup, item) => {
+            (relationsTypeGroup[item[key][innerKey]] = relationsTypeGroup[item[key][innerKey]] || []).push(item);
+            return relationsTypeGroup;
+        }, {});
+    }
+
+    checkForRelationShipDetails() {
+        return !isEmptyObject(this.relationshipDetails);
+    }
+
+    getIcon(key: string): string {
+        switch (key) {
+            case 'Commitment': return 'handshake';
+            case 'Travel': return 'flight';
+            case 'Financial': return 'paid';
+            default: return;
+        }
+    }
 
 }
