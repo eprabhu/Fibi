5fb736dfa4f4b24ae8dbf91738bbfb37e5717f4d
anishantonyk <itzanishkantony@gmail.com>
2024-07-26 09:22:26 +0100

    Fix of DnB Token Service

--- Server/fibi-coi-connect/src/main/java/com/polus/integration/entity/apitokenservice/DnBAPITokenClient.java
+++ Server/fibi-coi-connect/src/main/java/com/polus/integration/entity/apitokenservice/DnBAPITokenClient.java
@@ -23,21 +23,15 @@ public class DnBAPITokenClient implements ThirdPartyApiClient{
 	@Override
 	public TokenResponseDTO getNewToken() {
 		try {
-            // API endpoint
-            String endpoint = "https://plus.dnb.com/v3/token";//
-            //"https://plus.dnb.com/v3/token"; //
-            // Username and password
-            String username = "";
-            String password = "";
             
-            // Encode username and password in Base64
-            String auth = username + ":" + password;
+            String endpoint = getAuthTokenURL();
+            String customerKey = "";
+            String customerSecret = "";
+           
+            String auth = customerKey + ":" + customerSecret;
             String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
 
-            // Create HttpClient
             HttpClient client = HttpClient.newHttpClient();
-
-            // Create HttpRequest
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(endpoint))
                     .header("Content-Type", "application/x-www-form-urlencoded")
@@ -46,20 +40,14 @@ public class DnBAPITokenClient implements ThirdPartyApiClient{
                     .POST(HttpRequest.BodyPublishers.ofString("grant_type=client_credentials"))
                     .build();
 
-            // Send the request and get the response
+            
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             ObjectMapper objectMapper = new ObjectMapper();
-            
-            // Convert JSON string to TokenResponseDTO object
             TokenResponseDTO tokenResponse = objectMapper.readValue(response.body(), TokenResponseDTO.class);
             System.out.println("Access Token: " + tokenResponse.getAccessToken());
-            // Print the response code
             System.out.println("Response Code: " + response.statusCode());
-
-            // Print the response body
-            System.out.println("Response Body: " + response.body());
-            
+            System.out.println("Response Body: " + response.body());            
             return tokenResponse;
 
         } catch (Exception e) {
@@ -69,7 +57,8 @@ public class DnBAPITokenClient implements ThirdPartyApiClient{
 	}
 	
 	private String getAuthTokenURL() {
-		return urlConfig.getAuthToken();
+		//return urlConfig.getAuthToken();
+		return "https://plus.dnb.com/v3/token";
 	}
 
 }
