070046ae338aaa01044c1edfdf923d7f9aaa9db6
kiran.s <kiran.s@DESKTOP-LQSDRJA.polus.com>
2024-08-08 10:11:04 +0530

    Commit for view change in fibi base

---
+++ DB/FIBI-COI-5.0.4/create_views.sql
@@ -0,0 +1,12 @@
+
+DROP VIEW IF EXISTS coi_project_proposal_status_v;
+DROP VIEW IF EXISTS coi_project_proposal_qnr_ans_v;
+DROP VIEW IF EXISTS coi_project_institute_proposal_v;
+DROP VIEW IF EXISTS coi_project_proposal_v;
+DROP VIEW IF EXISTS coi_project_award_v;
+
+\. ./Views/coi_project_proposal_status_v.sql
+\. ./Views/coi_project_proposal_qnr_ans_v.sql
+\. ./Views/coi_project_institute_proposal_v.sql
+\. ./Views/coi_project_proposal_v.sql
+\. ./Views/coi_project_award_v.sql
