c57235cbc11da197c09350b197fd57ba0e625e33
SarumathiPolus <sarumathi.n@polussoftware.com>
2024-09-20 19:36:28 +0530

    IsSaving issue
    Bug fix.

--- Client/projects/coi/src/app/entity-management-module/entity-overview/additional-addresses/additional-addresses.component.ts
+++ Client/projects/coi/src/app/entity-management-module/entity-overview/additional-addresses/additional-addresses.component.ts
@@ -202,30 +202,27 @@ export class AdditionalAddressesComponent implements OnInit, OnDestroy {
     }
 
     editIndustry() {
-        if (!this.isSaving) {
+        this.entityMandatoryValidation();
+        if (!this.mandatoryList.size && !this.isSaving) {
             this.isSaving = true;
-            this.entityMandatoryValidation();
-            if (!this.mandatoryList.size) {
-                this.$subscriptions.push(this._entityOverviewService.updateAdditionalAddresses(this.additionalAddressObj).subscribe((res: any) => {
-                    this.additionalAddresses[this.isEditIndex].addressTypeCode = this.additionalAddressObj.addressTypeCode;
-                    this.additionalAddresses[this.isEditIndex].addressLine1 = this.additionalAddressObj.addressLine1;
-                    this.additionalAddresses[this.isEditIndex].addressLine2 = this.additionalAddressObj.addressLine2;
-                    this.additionalAddresses[this.isEditIndex].countryCode = this.additionalAddressObj.countryCode;
-                    this.additionalAddresses[this.isEditIndex].country = this.selectedCountry;
-                    this.additionalAddresses[this.isEditIndex].entityAddressType = this.selectAddressType;
-                    this.additionalAddresses[this.isEditIndex].city = this.additionalAddressObj.city;
-                    this.additionalAddresses[this.isEditIndex].state = this.additionalAddressObj.state;
-                    this.additionalAddresses[this.isEditIndex].postCode = this.additionalAddressObj.postCode;
-                    this._dataStorService.enableModificationHistoryTracking();
-                    this.updateDataStore();
-                    this.clearAdditionalAddress();
-                    this.isSaving = false;
-                }, err => {
-                    this._commonService.showToast(HTTP_ERROR_STATUS, 'Something went wrong. Please try again.');
-                    this.isSaving = false;
-                }));
-            }
-
+            this.$subscriptions.push(this._entityOverviewService.updateAdditionalAddresses(this.additionalAddressObj).subscribe((res: any) => {
+                this.additionalAddresses[this.isEditIndex].addressTypeCode = this.additionalAddressObj.addressTypeCode;
+                this.additionalAddresses[this.isEditIndex].addressLine1 = this.additionalAddressObj.addressLine1;
+                this.additionalAddresses[this.isEditIndex].addressLine2 = this.additionalAddressObj.addressLine2;
+                this.additionalAddresses[this.isEditIndex].countryCode = this.additionalAddressObj.countryCode;
+                this.additionalAddresses[this.isEditIndex].country = this.selectedCountry;
+                this.additionalAddresses[this.isEditIndex].entityAddressType = this.selectAddressType;
+                this.additionalAddresses[this.isEditIndex].city = this.additionalAddressObj.city;
+                this.additionalAddresses[this.isEditIndex].state = this.additionalAddressObj.state;
+                this.additionalAddresses[this.isEditIndex].postCode = this.additionalAddressObj.postCode;
+                this._dataStorService.enableModificationHistoryTracking();
+                this.updateDataStore();
+                this.clearAdditionalAddress();
+                this.isSaving = false;
+            }, err => {
+                this._commonService.showToast(HTTP_ERROR_STATUS, 'Something went wrong. Please try again.');
+                this.isSaving = false;
+            }));
         }
     }
 
