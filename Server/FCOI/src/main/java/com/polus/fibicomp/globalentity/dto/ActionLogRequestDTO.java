e57cf0f53ccc9c930d7225aabca8e1f93129a1f2
kiran.s <kiran.s@DESKTOP-LQSDRJA.polus.com>
2024-09-23 13:03:51 +0530

    Commit for fixes in entity history tab

--- Server/FCOI/src/main/java/com/polus/fibicomp/globalentity/dto/ActionLogRequestDTO.java
+++ Server/FCOI/src/main/java/com/polus/fibicomp/globalentity/dto/ActionLogRequestDTO.java
@@ -1,5 +1,7 @@
 package com.polus.fibicomp.globalentity.dto;
 
+import java.sql.Timestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,19 @@ public class ActionLogRequestDTO {
 
 	private Integer entityId;
 	private Integer entityNumber;
+	private Integer entityRiskId;
 	private String entityName;
 	private String updatedBy;
 	private String dunsNumber;
 	private String tabName;
 	private String actionLogCode;
+	private String riskType;
+	private String oldRiskLevel;
+	private String newRiskLevel;
+	private String oldComment;
+	private String newComment;
+	private String oldFeedStatus;
+	private String newFeedStatus;
+	private Timestamp updateTimestamp;
 
 }
