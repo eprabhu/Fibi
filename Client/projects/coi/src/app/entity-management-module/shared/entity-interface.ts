f016edfd9d08a9866dc625729c9f26f59749e35f
Abdul Ashfaque M. <abdul.a@polussolutions.com>
2024-10-07 16:21:37 +0530

    FC-968 - Entity Duplication Prevention in Verification Pop-up
    FC-968 - Entity Duplication Prevention in Verification Pop-up
    https://polussolutions.atlassian.net/browse/FC-968
    
    -- Abdul Ashfaque M

--- Client/projects/coi/src/app/entity-management-module/shared/entity-interface.ts
+++ Client/projects/coi/src/app/entity-management-module/shared/entity-interface.ts
@@ -1,6 +1,12 @@
+import { ModalActionEvent } from "../../shared-components/coi-modal/coi-modal.interface";
+
 export type EntityRiskProxyController = '/organization' | '/sponsor' | '/compliance' | '';
 export type EntityRiskCategoryCode = 'OR' | 'EN' | 'CO' | 'SP' | '';
-export const ENTITY_DUPLICATE_MATCH_MODAL_ID = 'duplicate_entity_match_found';
+export type VerifyActionType = 'API_FAILED' | 'VIEW_DUPLICATE' | 'VIEW_SPONSOR' | 'VIEW_SUBAWARD' | 'VIEW_OVERVIEW';
+export type DuplicateActionType = 'CLOSE_BTN' | 'SECONDARY_BTN' | 'PRIMARY_BTN' | 'NOT_FOUND' | 'CHECK_BOX' | 'CLOSE_SLIDER' | 'API_FAILED';
+export type VerifyModalAction = ModalActionEvent | { event?: any, action: VerifyActionType };
+export const ENTITY_DUPLICATE_MATCH_MODAL_ID = 'duplicate_entity_match_found_modal';
+export const ENTITY_DUPLICATE_MATCH_SLIDER_ID = 'duplicate_entity_match_found_slider';
 
 export class Create_Entity {
     entityName: string = '';
@@ -506,17 +512,22 @@ export class EntityCardDetails {
 }
 
 export class DuplicateCheckObj{
-    entityName : string;
-    primaryAddressLine1: string;
-    primaryAddressLine2: string;
-    countryCode: string;
+    entityName?: string;
+    primaryAddressLine1?: string;
+    primaryAddressLine2?: string;
+    countryCode?: string;
 }
 
-export class EntityDupCheckConfig{
-    duplicateView: 'MODAL_VIEW' | 'CARD_VIEW' = 'MODAL_VIEW';
-    modalHeader?: string = 'Matching Entities Found'; //based on mode optional
-    modalPrimaryButton?: string = 'Create New';
-    modalHelpText?: string = `The details you entered match the following entities in our system. Please review the list below. If you still wish to create a new entity, you can skip this step and click on '${this.modalPrimaryButton}'.`;
+export class EntityDupCheckConfig {
+    duplicateView: 'MODAL_VIEW' | 'CARD_VIEW' | 'SLIDER_VIEW' | '' = 'MODAL_VIEW';
+    header?: string = 'Matching Entities Found'; //based on mode optional
+    helpTextModuleCode? = '';
+    primaryButton?: string = 'Create New';
+    confirmationText? = '';
+    hasConfirmedNoDuplicate? = false;
+    entityIdToFilter?: number = null;
+    triggeredFrom?: 'CREATE_ENTITY' | 'ENTITY_VERIFY' | '' = '' 
+    infoText?: string = `The details you entered match the following entities in our system. Please review the list below. If you still wish to create a new entity, you can skip this step and click on '${this.primaryButton}'.`;
 }
 
 export class DupMarkingModalConfig {
@@ -562,6 +573,16 @@ export class SponsorFields {
     sponsorTypeCode?: any;
     acronym?: string;
     feedStatusCode?: any;
+}
+export class DNBReqObj {
+    sourceDataName: string;
+    sourceDunsNumber: any;
+    emailAddress: string;
+    addressLine1: string;
+    addressLine2: string;
+    postalCode: string;
+    state: string;
+    countryCode: string;
 }
 
 //changes during modification in the following fields need to update feed status of sponsor
